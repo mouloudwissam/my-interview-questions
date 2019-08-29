de1cad44478fa3ae03c0671944c0bda554d3496c
mouloudwissam <achfrimouloud@gmail.com>
2019-08-29 00:16:56 -0400

    PFIQ

---
+++ PracticeForInteviewQuestions/src/firstPake/NarrowCosting.java
@@ -0,0 +1,47 @@
+package firstPake;
+
+public class NarrowCosting {
+	public static void main(String[] args) {
+		double Myname = 8.1234;
+		int Mynamex = (int) Myname;
+		System.out.println(Myname);
+		System.out.println(Mynamex);
+		float MyFloat = 12.6454748048f;
+		long MyLOng = (long) MyFloat;
+		System.out.println(MyFloat);
+		System.out.println(MyLOng);
+		boolean MyBoolean = true;
+		boolean Mybeaty = false;
+		// Operator (+ - */......)
+		// Operand ( is primative or non-primative in other words is the Name of the
+		// Value).
+		String MyName = "hassanmou\"loud\"jamal";
+		String Mylastname = "AC\'HF\"RI";
+		int x = 3;
+		int y = 4;
+		int xx = 12;
+		int yy = 13;
+		System.out.println(" THE LENGHT OF THE MyName :" + MyName.length());
+		String Myword = "hassan.mouloud.jamal.care of each others";
+		System.out.println("the best leght of:" + Myword.length());
+		System.out.println(MyName.toUpperCase());
+		System.out.println(Myword.toUpperCase());
+		System.out.println(Mylastname.toLowerCase());
+		System.out.println(Myword.indexOf('c'));
+		System.out.println(MyName.concat(Mylastname));
+		System.out.println(MyName + "  " + Mylastname);
+		System.out.println(MyName);
+		System.out.println(Mylastname);
+
+		System.out.println(Math.max(x, y));
+		System.out.println(Math.min(x, y));
+		System.out.println(Math.max(xx, yy));
+		System.out.println(Math.sqrt(x));
+		System.out.println(Math.abs(yy));
+		System.out.println(Math.abs(x));
+		System.out.println(Math.sqrt(y));
+		System.out.println(Math.random());
+		System.out.println(MyBoolean);
+		System.out.println(Mybeaty);
+	}
+}
---
+++ PracticeForInteviewQuestions/src/firstPake/firstCosting.java
@@ -0,0 +1,15 @@
+package firstPake;
+
+public class firstCosting {
+	
+public static void main(String[] args) {
+	int MyNumber=12;
+	double mySister = MyNumber;
+	float myFLOATONE = 12.435363735278825f;
+	System.out.println(MyNumber);
+	System.out.println(mySister);
+	System.out.println(myFLOATONE);
+	System.out.println(MyNumber - myFLOATONE);
+}
+
+}
